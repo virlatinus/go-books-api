0aaaeaa4b28a5e7f6b18867ad2cd3a2857f025e1
Werner Garcia <werner.garcia@gmail.com>
2024-08-26 22:40:42 -0600

    Add .env file support

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/MadAppGang/httplog"
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
@@ -13,7 +15,7 @@ import (
 
 type Book struct {
 	gorm.Model
-	Title  string `json:"title" gorm:"not null,unique,index:title_idx"`
+	Title  string `json:"title" gorm:"not null;uniqueIndex:title_idx"`
 	Author string `json:"author" gorm:"not null"`
 	Year   int    `json:"year" gorm:"not null"`
 }
@@ -21,9 +23,24 @@ type Book struct {
 var db *gorm.DB
 
 func main() {
-	router := mux.NewRouter()
 	var err error
-	dsn := "books:books@tcp(localhost:3306)/books?charset=utf8mb4&parseTime=True&loc=Local"
+
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(".")
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("error reading config file: %s", err))
+	}
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		viper.Get("DB_USER"),
+		viper.Get("DB_PASS"),
+		viper.Get("DB_HOST"),
+		viper.Get("DB_PORT"),
+		viper.Get("DB_NAME"),
+	)
 	db, err = gorm.Open(mysql.New(mysql.Config{
 		DSN:               dsn,
 		DefaultStringSize: 256, // default size for string fields
@@ -38,10 +55,12 @@ func main() {
 		panic(err)
 	}
 
+	router := mux.NewRouter()
 	router.Use(httplog.Logger)
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
 	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books/batch", addBooks).Methods("POST")
 	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 
@@ -99,12 +118,34 @@ func addBook(w http.ResponseWriter, r *http.Request) {
 
 	err = db.Create(&book).Error
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusConflict)
 		_, _ = w.Write([]byte(err.Error()))
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+func addBooks(w http.ResponseWriter, r *http.Request) {
+	var books []Book
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&books)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	result := db.Create(&books)
+	if result.Error != nil {
+		w.WriteHeader(http.StatusConflict)
+		_, _ = w.Write([]byte(result.Error.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(fmt.Sprintf("{\"message\": \"%d books added\"}\n", result.RowsAffected)))
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
@@ -134,7 +175,7 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 
 	err = db.Save(&book).Error
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusConflict)
 		_, _ = w.Write([]byte(err.Error()))
 		return
 	}
